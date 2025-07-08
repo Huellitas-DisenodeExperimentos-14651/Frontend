fe60b56a38d2c50c78d64b01e5bb23ebf8c44763
Diegoroip <rosadoiporre@gmail.com>
2025-07-08 11:19:32 -0500

    feat: cambios para el main, front conectado al backend en Render

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.prod.ts <-- para producción en Render
-export const environment = {
-  production: true,
-  serverBasePath: "https://patita-solidaria-backend.onrender.com/api/v1"
-};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://patita-solidaria-backend.onrender.com'
+  apiUrl: 'https://patita-solidaria-backend.onrender.com',
+  serverBasePath: 'https://patita-solidaria-backend.onrender.com/api/v1'
 };
